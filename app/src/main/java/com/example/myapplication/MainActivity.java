bbd153493a67146425c8fb3a4234ec5d2313a77c
limingyue <v-limingyue@xiaomi.com>
2019-06-21 09:14:25 +0800

    activity study

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,26 +9,24 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Button but;
-    TextView tv;
 
     @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        but = findViewById(R.id.btn);
-        but.setOnClickListener(new View.OnClickListener() {
+        Button btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
-                intent.setClass(MainActivity.this, Activity2.class);
+                intent.setClass(MainActivity.this, SecondActivity.class);
                 startActivity(intent);
 
             }
         });
-        tv = findViewById(R.id.tv);
-        tv.setText(" hello world1 ");
+        TextView tv = findViewById(R.id.tv);
+        tv.setText("MainActivity");
 
     }
 }
